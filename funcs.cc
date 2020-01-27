1ad27d4a50e27837cb1207efc6efc82d64137967
Daniel Zatko <dzatko@stevens.edu>
2020-01-27 15:19:24 -0500

    Update

--- funcs.cc
+++ funcs.cc
@@ -9,7 +9,7 @@
 #include <cstdint>
 using namespace std;
 uint32_t sum(uint32_t a, uint32_t b);
-uint64_t prod(uint32_t a, uint32_t b); // Frank Pinnola
+uint64_t prod(uint32_t a, uint32_t b);
 uint32_t sumsq(uint32_t a, uint32_t b);
 uint32_t countPrimes(uint32_t a, uint32_t b);
 bool isPrime(uint32_t p);//Aidan Graffam
@@ -18,10 +18,10 @@ uint32_t gcd(uint32_t a, uint32_t b);
 uint32_t lcm(uint32_t a, uint32_t b);
 double fact(uint32_t n);
 uint64_t sum(uint32_t n);
-double hypot(double a, double b);
+double hypot(double a, double b);//Daniel Zatko
 double diffsq(double a, double b);
 double mean(int a, int b); //Vincent Schlenker
-double mean(int a, int b, int c); //Jakob Niglio
+double mean(int a, int b, int c);
 bool pythagoreantriple(double a, double b);
 double trigIdentity(double x);
 /*
@@ -70,7 +70,7 @@ int min(int x[], int n);
 double prod(int x[], int n);
 int sum(int x[], int n);
 void demean(double x[], int n);
-void normalize(double x[], int n); //Frank Pinnola
+void normalize(double x[], int n);
 void round(double x[], int n);
 void square(double x[], int n);
 void squareRoot(double x[], int n);
@@ -81,7 +81,7 @@ uint32_t countEvens(const int x[], int n);
 void addToEach(int x[], int n, int delta);
 
 void removeVowels(char s[]);
-void reverse(char s[]);
+void reverse(char s[]);//Daniel Zatko
 bool isPalindrome(const char s[]);
 uint32_t checksum(const char s[]);
 uint32_t myhash(const char s[]);
