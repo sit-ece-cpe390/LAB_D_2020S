d0e3a6b9a27150b44652d37792546800cc9911d5
John Siyaga <johnsiyaga@gmail.com>
2020-01-27 15:16:13 -0500

    Selected functions

--- funcs.cc
+++ funcs.cc
@@ -21,7 +21,7 @@ uint64_t sum(uint32_t n);
 double hypot(double a, double b);
 double diffsq(double a, double b);
 double mean(int a, int b); //Vincent Schlenker
-double mean(int a, int b, int c);
+double mean(int a, int b, int c); // John Siyaga
 bool pythagoreantriple(double a, double b);
 double trigIdentity(double x);
 /*
@@ -75,7 +75,7 @@ void round(double x[], int n);
 void square(double x[], int n);
 void squareRoot(double x[], int n);
 uint32_t strip(double x[], uint32_t n, double a, double b);
-void reverse(int x[], int n);
+void reverse(int x[], int n); // John Siyaga
 uint32_t randomElement(const int x[], int n);
 uint32_t countEvens(const int x[], int n);
 void addToEach(int x[], int n, int delta);
