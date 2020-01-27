195ba287b5a82a817c08948542a17a428ccfbbe5
Dominic Zecchino <56906091+dzecchino@users.noreply.github.com>
2020-01-27 18:00:20 -0500

    Update funcs.cc

--- funcs.cc
+++ funcs.cc
@@ -11,7 +11,7 @@
 using namespace std;
 uint32_t sum(uint32_t a, uint32_t b);
 uint64_t prod(uint32_t a, uint32_t b);
-uint32_t sumsq(uint32_t a, uint32_t b);
+uint32_t sumsq(uint32_t a, uint32_t b); //Dominic Zecchino
 uint32_t countPrimes(uint32_t a, uint32_t b); //Michael Iafelice
 bool isPrime(uint32_t p);//Aidan Graffam
 void swap(uint32_t& a, uint32_t& b);
@@ -68,7 +68,7 @@ double grav(double m1, Vec3d v1, double m2, Vec3d v2);
 double mean(int x[], int n); //Sean Jahnige
 int max(int x[], int n); //Michael Iafelice
 int min(int x[], int n); //Max Perez
-double prod(int x[], int n);//Nicholas Lenge
+double prod(int x[], int n);//Dominic Zecchino
 int min(int x[], int n); // Christina Berke
 double prod(int x[], int n); //Patrick Biesty
 int sum(int x[], int n);
