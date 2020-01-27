28e6ee2e4cc55d4366100ba4b3e370aa3b31ffa2
Brandon Llizo <bllizo@stevens.edu>
2020-01-27 15:50:49 -0500

    BL

--- funcs.cc
+++ funcs.cc
@@ -18,11 +18,7 @@ void swap(uint32_t& a, uint32_t& b); //Max Perez
 uint32_t gcd(uint32_t a, uint32_t b);
 void swap(uint32_t& a, uint32_t& b);
 uint32_t gcd(uint32_t a, uint32_t b); //David Barlow
-uint32_t lcm(uint32_t a, uint32_t b) { //Brandon Llizo
-	int product = a * b;
-	int lcm = product / gcd(a, b);
-	return lcm;
-}
+uint32_t lcm(uint32_t a, uint32_t b); //Brandon Llizo
 double fact(uint32_t n); //Sean Jahnige
 uint64_t sum(uint32_t n); //Jose Fermin
 double hypot(double a, double b);//Daniel Zatko
